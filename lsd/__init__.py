501dd7cd52287a5cb42b677e16b253e11341fb59
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-07-07 20:21:46 -0700

    General cleanup.

--- lsd/__init__.py
+++ lsd/__init__.py
@@ -104,15 +104,11 @@ def create_app(config, config_overrides=dict()):
     def expected_shell_imports():
         from pprint import pprint
         from .cloud_log import CloudLog, LowPassFilter, StreamClient, StreamTransport
-        # from .node_tree import LogNode, RootLogNode, handler_ranges, get_tree, make_tree, all_nodes
         import inspect
 
         app.try_trigger_before_first_request_functions()
         logDict = logging.root.manager.loggerDict
         all_loggers = [logger for name, logger in logDict.items()]
-        # all_loggers = [getattr(app, name, None) for name in app.log_list]
-        # all_loggers = [logging.root, app.logger] + [log for log in all_loggers if log]
-        # tree = make_tree(all_loggers)
 
         return {
             'pprint': pprint,
@@ -122,13 +118,6 @@ def create_app(config, config_overrides=dict()):
             'StreamTransport': StreamTransport,
             'all_loggers': all_loggers,
             'logDict': logDict,
-            # 'LogNode': LogNode,
-            # 'RootLogNode': RootLogNode,
-            # 'handler_ranges': handler_ranges,
-            # 'get_tree': get_tree,
-            # 'make_tree': make_tree,
-            # 'tree': tree,
-            # 'all_nodes': all_nodes,
             'logging': logging,
             'inspect': inspect,
             }
