a35172d4aae47caf3b1ca70ed9ccad994b80e5a2
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-07-22 11:13:52 -0700

    Moving and referencing module descriptors.

--- src/cloudlog/__init__.py
+++ src/cloudlog/__init__.py
@@ -2,7 +2,15 @@ from .cloud_log import CloudLog, CloudParamHandler, IgnoreFilter, LowPassFilter,
 from .log_helpers import get_named_handler, move_handlers, setup_warnings_log
 from .range_helpers import levels_covered
 
+__module_name__ = 'cloudlog'
 __version__ = '0.1.0'
+__author__ = 'Chris L Chapman'
+__email__ = 'Chris@SeattleCode.dev'
+__license__ = 'MIT'
+__repository__ = 'https://github.com/seattlechris/cloudlog'
+__description__ = """Expanded logging features on GCP - GAE with google.cloud.logging
+                     [Google Cloud Platform - Google App Engine]."""
+
 # __all_ = [
 #     'CloudLog',
 #     'CloudParamHandler',
